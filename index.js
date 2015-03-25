af1aef6cb5bdce900d363c1d6c1cdcd0496a233a
impvhc <impvhc@homail.com>
2015-03-25 11:36:43 -0700

    pruebas

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var selector = require('./selector.js');
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
